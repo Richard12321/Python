3221b353faf6a8b0f54420bc72443f360cc0241c
Richard12321 <rzallinger@gmx.at>
2022-09-28 12:32:36 +0200

    Lotto Fertig

--- lotto.py
+++ lotto.py
@@ -1,30 +1,30 @@
 import random
 
-def lottoziehung():
+def lottoziehung(zahlenvon, zahlenbis, gezogene):
     zahlen = []
-    for i in range(45):
-        zahlen.append(i+1)
+    for i in range(zahlenbis-zahlenvon):
+        zahlen.append(zahlenvon+i+1)
 
-    for i in range(6):
-        zufallszahl = int(random.random() * (45-i)) + 1
-        zahlen[44-i] = zahlen[zufallszahl-1]
-        zahlen[zufallszahl-1] = 45-i
+    for i in range(gezogene):
+        zufallszahl = random.randrange(zahlenvon, zahlenbis)
+        zahlen[len(zahlen)-i-1], zahlen[zufallszahl-zahlenbis] = zahlen[zufallszahl-zahlenbis], zahlen[len(zahlen)-i-1]
 
-    return zahlen[39:]
+    return zahlen[len(zahlen)-gezogene:]
 
-def lottoziehung_statistik():
+def lottoziehung_statistik(zahlenvon, zahlenbis, gezogene, ziehungen):
     dict = {}
 
-    for i in range(45):
-        dict[i+1] = 0
+    for i in range(zahlenbis-zahlenvon):
+        dict[zahlenvon+i+1] = 0
 
-    for i in range(1000):
-        zahlen = lottoziehung()
+    for i in range(ziehungen):
+        zahlen = lottoziehung(zahlenvon,zahlenbis, gezogene)
         for j in zahlen:
             dict[j] = dict[j] + 1
 
     for i in dict:
         print(str(i) + ": " + str(dict[i]))
 
-print(lottoziehung())
-lottoziehung_statistik()
+
+print(lottoziehung(0,45,6))
+lottoziehung_statistik(0,45,6,1000)
